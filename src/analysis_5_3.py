fa8905c3e094f92a2c64a04a36dbe2648ca1c126
Pimkanit Thongsrikaew <tanpimm@Tanpimms-Pro-M1.local>
2025-11-28 21:34:55 +0700

    update latest

--- src/analysis_5_3.py
+++ src/analysis_5_3.py
@@ -9,7 +9,7 @@ from analysis_queries import query_to_df
 # ----------------------------------------------------------
 # 0) Configure Gemini API
 # ----------------------------------------------------------
-genai.configure(api_key="AIzaSyB3lOs8_0YL8Xzux_Qac_5WbgaMoeRUhzs")
+genai.configure(api_key="AIzaSyAWNc_hcQJuM9kzJhLLM6h8iIOH8tnSRaI")
 
 # เลือกโมเดลที่รองรับ generate_content() บนระบบของผู้ใช้
 MODEL_NAME = "models/gemini-2.5-flash"
